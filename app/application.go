7528d2122c61406c21fbef723553281c56675fb3
Armingodiz <armingodarzi1380@gmail.com>
2021-02-11 17:28:58 +0330

    startApplication function done

--- app/application.go
+++ app/application.go
@@ -1,5 +1,16 @@
 package app
 
-func StartApplication()  {
-	
+import "github.com/gin-gonic/gin"
+
+var (
+	// router will be in charge of creating different goRoutines for different request we want to handle .
+	router = gin.Default() // a private var for app package
+)
+
+func StartApplication() {
+	mapUrls()
+	err := router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -3,10 +3,16 @@ package controllers
 // provide endpoints to interact with users API
 // every request should be handled by the controller , so this is our entry point of our application
 
+// handle every request for creating user
+func CreateUser() {
+
+}
+
+// handle every request for getting user from db
 func GetUser() {
 
 }
 
-func CreateUser() {
+func FindUser() {
 
 }
