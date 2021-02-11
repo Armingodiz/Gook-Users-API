dc5d6be79ee3ca9e3e518d52bca4ff411abb3ee2
Armingodiz <armingodarzi1380@gmail.com>
2021-02-11 17:16:59 +0330

    working on service structure based on mvc design pattern

---
+++ controllers/users_controller.go
@@ -0,0 +1,12 @@
+package controllers
+
+// provide endpoints to interact with users API
+// every request should be handled by the controller , so this is our entry point of our application
+
+func GetUser() {
+
+}
+
+func CreateUser() {
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ArminGodiz/Gook-Users-API/app"
+
+func main()  {
+	app.StartApplication()
+}
