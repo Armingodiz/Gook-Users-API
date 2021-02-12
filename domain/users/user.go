a9de7b16a27731567dd0ed8c28a0b9346804ef36
Armingodiz <armingodarzi1380@gmail.com>
2021-02-12 12:29:44 +0330

    Validate method added for user

--- domain/users/user.go
+++ domain/users/user.go
@@ -1,9 +1,22 @@
 package users
 
+import (
+	"github.com/ArminGodiz/Gook-Users-API/utils/errors"
+	"strings"
+)
+
 type User struct {
 	Id          int64  `json:"id"`
 	FirsName    string `json:"first_name"`
 	LastNAme    string `json:"last_name"`
 	Email       string `json:"email"`
 	DateCreated string `json:"date_created"`
+}
+
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address !")
+	}
+	return nil
 }
--- services/users_service.go
+++ services/users_service.go
@@ -6,5 +6,8 @@ import (
 )
 
 func CreatUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
