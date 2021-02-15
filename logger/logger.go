8a89a0350bae2cb4f75ce2bc2233ecff140fcf3e
Armingodiz <armingodarzi1380@gmail.com>
2021-02-15 14:43:36 +0330

    logger configiration added

--- logger/logger.go
+++ logger/logger.go
@@ -1,3 +1,30 @@
 package logger
 
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
+var (
+	Log *zap.Logger
+)
+
+func init() {
+	logConfig := zap.Config{
+		OutputPaths: []string{"stdout"},
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "msg",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+	var err error
+	if Log, err = logConfig.Build(); err != nil {
+		panic(err)
+	}
+}
